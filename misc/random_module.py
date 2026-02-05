f961e73efb1e8efa0dab31ac50872417d4fec1f4
Samvel Pahalyants <spahalyants@gmail.com>
2026-02-05 16:00:42 +0300

    Add Number Guessing Game

---
+++ misc/random_module.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(1, 6))   # dice roll
+print(random.choice(["a", "b", "c"]))
+
+print(random.randint(1, 6))   # dice roll
+print(random.choice(["a", "b", "c"]))
+
+print(random.randint(1, 6))   # dice roll
+print(random.choice(["a", "b", "c"]))
+
+print(random.randint(1, 6))   # dice roll
+print(random.choice(["a", "b", "c"]))
