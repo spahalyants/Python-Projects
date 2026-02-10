bbbd3d26ccc8d1262f010c741d2c71e7ebc8fea3
Samvel Pahalyants <spahalyants@gmail.com>
2026-02-10 13:33:10 +0300

    Built-in functions in Python (intro - until LC #18)

---
+++ bg-python/intro.py
@@ -0,0 +1,18 @@
+def my_fn(a,b):
+    a = a + b
+    c = a + b
+    return c
+
+print(my_fn(3,4))
+
+print (print)
+
+word = "universe"
+
+print(dir(word))
+
+print(word.upper())
+
+####################
+# Comments in Python
+####################
