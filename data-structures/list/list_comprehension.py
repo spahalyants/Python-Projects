ad7896435679dd65f7163d322cce7e531ca49381
Samvel Pahalyants <spahalyants@gmail.com>
2026-02-17 13:38:25 +0300

    list comprehension
    # Conflicts:
    #	data-structures/list/list_comprehension.py

--- data-structures/list/list_comprehension.py
+++ data-structures/list/list_comprehension.py
@@ -7,7 +7,17 @@ items = [("a", 10), ("b", 9), ("c", 12)]
 
 x = [item[1] for item in items]
 
-print(x)
+print(x)     # [10, 9, 12]
+
+
+items = [("a", 10), ("b", 9), ("c", 12)]
+
+items.sort(key=lambda item: item[1])  # sort by price
+print(items)
+
+items.sort(key=lambda item: item[0])  # sort by product name
+print(items)
+
 
 
 
