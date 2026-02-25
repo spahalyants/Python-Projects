e44de844443447100a0f65b99f640cfba7dff613
Samvel Pahalyants <spahalyants@gmail.com>
2026-02-25 18:15:55 +0300

    loops and char v2

--- experiments/exp_03_loops_1.py
+++ experiments/exp_03_loops_1.py
@@ -1,8 +1,9 @@
-for i in range(10):
-    print(i, end=" ")
-
-print()
-
-for i in range(65, 91):
+# Digits 0-9
+for i in range(48, 58):
     print(chr(i), end=" ")
+# Output: 0 1 2 3 4 5 6 7 8 9
 
+# Uppercase letters A-J
+for i in range(65, 75):
+    print(chr(i), end=" ")
+# Output: A B C D E F G H I J
