49f08beb867a2cda5dd34768af48cd1b547253a2
Samvel Pahalyants <spahalyants@gmail.com>
2026-02-27 15:27:47 +0300

    del list

---
+++ test/test_03.py
@@ -0,0 +1,22 @@
+data = {
+    "user_id": 1042,
+    "username": "blue_fox",
+    "email": "blue.fox@example.com",
+    "age": 29,
+    "is_active": True,
+    "score": 87.5,
+    "country": "USA",
+    "city": "New York",
+    "favorite_colors": ["navy", "orange", "purple"],
+    "last_login": "2026-02-26T21:18:00-05:00",
+}
+
+print(1)
+
+list = [1, 2, 3]
+print(list)
+
+a = 10
+
+
+
